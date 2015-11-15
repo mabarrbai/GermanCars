8fcdd054dd46840c0aee1378c0725ef69771f638
alex <beal_21@hotmail.com>
2015-11-15 13:36:16 +0100

    pachangas heroku

--- penhas/urls.py
+++ penhas/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from penhas import views
 
 urlpatterns = [
-	url(r'^$', fecha_actual),
+	url(r'^$', views.fecha_actual),
     url(r'^partido/equipos/$', views.formar_equipos),
     url(r'^penhas/$', views.lista_penha),
     url(r'^penhas/(?P<pk>[0-9]+)/$', views.penha_detail),
