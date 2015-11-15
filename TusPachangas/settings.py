a30045b41e9e26bdcfaae6b4d193353f0e1b8519
alex <beal_21@hotmail.com>
2015-11-15 13:45:19 +0100

    sqlite

--- TusPachangas/settings.py
+++ TusPachangas/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'TusPachangas.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -99,7 +99,13 @@ DATABASES = {
         'PORT':'5432'
     }
 }
-
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 ON_HEROKU = os.environ.get('PORT')
 if ON_HEROKU:
     DATABASE_URL='postgres://fhtyiztxysbrdn:WI7TLSaRrLYxotdwfryDYIQwn2@ec2-54-225-192-128.compute-1.amazonaws.com:5432/demv8ll9orcqeg'
