511c3b7868d0ed852b3168c971925c52a29d31d7
alex <beal_21@hotmail.com>
2016-02-06 13:35:22 +0100

    Despliegue Azure. Script despliegue

---
+++ scripts/deployAzure.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Herramientas
+sudo apt-get install nodejs-legacy
+sudo apt-get install npm
+sudo npm install -g azure-cli
+sudo pip install paramiko PyYAML jinja2 httplib2 ansible
+sudo wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb
+sudo dpkg -i vagrant_1.8.1_x86_64.deb
+vagrant plugin install vagrant-azure
+
+#Despliegue
+cd ../deployAzure/
+export ANSIBLE_HOSTS=~/ansible_hosts
+vagrant up --provider=azure
